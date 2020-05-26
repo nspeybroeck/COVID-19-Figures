42e459b069b5b43c4d250aa55a337d6361f7cb33
nspeybroeck <Niko.Speybroeck@uclouvain.be>
2020-05-26 16:59:25 +0200

    Update_Code_Mac_OK

--- plot_hosp_trends_divid_twographs.R
+++ plot_hosp_trends_divid_twographs.R
@@ -11,7 +11,7 @@ library(ggpubr)
 library(grid)
 library(gridExtra)
 
-# import Sciensano hospitaliszations data
+# import Sciensano hospitalisations data
 dat <- read.csv("https://epistat.sciensano.be/Data/COVID19BE_HOSP.csv")
 
 # aggregate new intakes by province and date
@@ -32,7 +32,7 @@ dat$PROVINCE <- factor(dat$PROVINCE,
     "BrabantWallon",
     "Brussels",
     "Hainaut",
-    "Liège",
+    "Li\xe8ge",
     "Limburg",
     "Luxembourg",
     "Namur",
@@ -75,12 +75,8 @@ dat <- dat %>%
   )) %>%
   mutate(NEW_IN_divid = NEW_IN / population * 100000)
 
-# create poisson function
-# poisson_smooth <- function(...) {
-#   geom_smooth(method = "glm", method.args = list(family = "Poisson"), ...)
-# }
 
-# Create plot in ndls/fr
+# Create plot in dutch/fr
 fig_trends <- ggplot(
   dat,
   aes(x = DATE, y = NEW_IN_divid)
@@ -94,16 +90,12 @@ fig_trends <- ggplot(
   facet_wrap(vars(PROVINCE),
     scales = "free"
   ) +
-  # poisson_smooth(
-  #   formula = y ~ splines::ns(x, 4),
-  #   se = FALSE,
-  #   col = "darkgrey"
-  # ) +
-  geom_smooth(se = FALSE,
-              col = "grey"
-              # , span=0.1
-             , method="gam", formula = y ~ s(x)
-          )+
+  geom_smooth(
+    se = FALSE,
+    col = "grey",
+    method = "gam",
+    formula = y ~ s(x)
+  ) +
   geom_vline(
     xintercept = as.Date("2020-05-04"), linetype = "dashed",
     color = "darkgrey", size = 0.5
@@ -146,24 +138,23 @@ fig_trends <- ggplot(
     alpha = .05
   ) +
   labs(
-  title = "Evolution des admissions hospitalières / Evolutie van de hospitalisaties - COVID-19"
+    title = "Evolution des admissions hospitalières / Evolutie van de hospitalisaties - COVID-19"
   ) +
   scale_y_continuous(breaks = seq(from = 0, to = 10, by = 2), limits = c(0, 10)) +
   scale_x_date(labels = date_format("%m-%Y"))
 
-fig_trends
-
-##adjust caption at the end of the trend figure
+## adjust caption at the end of the trend figure
 caption <- grobTree(
-                   textGrob(" * Lignes solides : courbes ajustées aux observations / Volle lijnen : gefitte curves \n * Lignes pointillées : phases de déconfinement 1a, 1b & 2 / Gestippelde lijnen: fases afbouw lockdown maatregelen 1a, 1b & 2",
-                            x=0, hjust=0, vjust=0, 
-                            gp=gpar(col="darkgray", fontsize=7, lineheight=1.2)), 
-                   textGrob("Niko Speybroeck (@NikoSpeybroeck), Antoine Soetewey (@statsandr) & Angel Rosas (@arosas_aguirre) \n Data: https://epistat.wiv-isp.be/covid/  ", 
-                            x=1, hjust=1, vjust=0, 
-                            gp=gpar(col="black",fontsize=7.5,lineheight=1.2)),
-                   cl="ann")
-
-
+  textGrob(" * Lignes solides : courbes ajustées aux observations / Volle lijnen : gefitte curves \n * Lignes pointillées : phases de déconfinement 1a, 1b & 2 / Gestippelde lijnen: fases afbouw lockdown maatregelen 1a, 1b & 2",
+    x = 0, hjust = 0, vjust = 0,
+    gp = gpar(col = "darkgray", fontsize = 7, lineheight = 1.2)
+  ),
+  textGrob("Niko Speybroeck (@NikoSpeybroeck), Antoine Soetewey (@statsandr) & Angel Rosas (@arosas_aguirre) \n Data: https://epistat.wiv-isp.be/covid/  ",
+    x = 1, hjust = 1, vjust = 0,
+    gp = gpar(col = "black", fontsize = 7.5, lineheight = 1.2)
+  ),
+  cl = "ann"
+)
 
 
 ##### MAPS
@@ -186,12 +177,13 @@ dat_ag <- dat %>%
   group_by(PROVINCE) %>%
   summarize(
     "new_in" = sum(NEW_IN, na.rm = T),
-    "new_in2"= sum(NEW_IN[DATE>="2020-05-04"], na.rm = T), 
+    "new_in2" = sum(NEW_IN[DATE >= "2020-05-04"], na.rm = T),
     "population" = max(population, na.rm = T)
-
   ) %>%
-  mutate(new_in_divid = new_in / population * 100000,
-         new_in_divid2 = new_in2 / population * 100000)
+  mutate(
+    new_in_divid = new_in / population * 100000,
+    new_in_divid2 = new_in2 / population * 100000
+  )
 
 map.data <- left_join(map, dat_ag, by = "PROVINCE")
 map.data <- subset(map.data, !PROVINCE %in% "Belgium")
@@ -219,14 +211,14 @@ points <- mutate(points,
 points1 <- subset(points, !PROVINCE %in% "Vlaams-Brabant")
 points2 <- subset(points, PROVINCE %in% "Vlaams-Brabant")
 
-period1<- paste0("Période / periode : 15/03 - ", format(Sys.Date()-1, format = "%d/%m"),"   ")
-period2<- paste0("Période / periode : 04/05 - ", format(Sys.Date()-1, format = "%d/%m"),"   ")
+period1 <- paste0("Période / periode : 15/03 - ", format(Sys.Date() - 1, format = "%d/%m"), "   ")
+period2 <- paste0("Période / periode : 04/05 - ", format(Sys.Date() - 1, format = "%d/%m"), "   ")
 
 
 
 fig_map1 <- ggplot(map.data) +
   geom_sf(aes(fill = new_in_divid)) +
-  #here you can change the number of blues in the pallete "n" (maximum=9)
+  # here you can change the number of blues in the pallete "n" (maximum=9)
   scale_fill_gradientn(colors = brewer.pal(n = 9, name = "Blues")) +
   geom_text(
     data = points1, aes(x = X, y = Y + 0.03, label = PROVINCE, colour = q1), size = 3,
@@ -245,15 +237,12 @@ fig_map1 <- ggplot(map.data) +
     data = points2, aes(x = X + 0.07, y = Y + 0.03, label = num_1), col = "black", size = 3,
     check_overlap = TRUE
   ) +
-
-  labs(fill=bquote(atop(NA, atop("Admissions hospitalières / \nHospitalisaties (x 100,000 hab./inw.)", bold(.(period1))))))+
-  #labs(fill=paste0("Admissions hospitalières / \nHospitalisaties (x 100,000 hab./inw.) \n", period1))+
+  labs(fill = bquote(atop(NA, atop("Admissions hospitalières / \nHospitalisaties (x 100,000 hab./inw.)", bold(.(period1)))))) +
   theme_void() +
   theme(
     # Change legend
     legend.position = c(0.2, 0.22),
     legend.title = element_text(size = 12, color = "black"),
-    #legend.title = element_text(size = 9, color = "black"),
     legend.text = element_text(color = "black"),
     plot.margin = unit(c(+0.2, 0, -0.5, 3), "cm")
   )
@@ -261,7 +250,7 @@ fig_map1 <- ggplot(map.data) +
 
 fig_map2 <- ggplot(map.data) +
   geom_sf(aes(fill = new_in_divid2)) +
-  #here you can change the number of blues in the pallete "n" (maximum=9)
+  # here you can change the number of blues in the pallete "n" (maximum=9)
   scale_fill_gradientn(colors = brewer.pal(n = 9, name = "Blues")) +
   geom_text(
     data = points1, aes(x = X, y = Y + 0.03, label = PROVINCE, colour = q2), size = 3,
@@ -280,28 +269,22 @@ fig_map2 <- ggplot(map.data) +
     data = points2, aes(x = X + 0.07, y = Y + 0.03, label = num_2), col = "black", size = 3,
     check_overlap = TRUE
   ) +
-
-  labs(fill=bquote(atop(NA, atop("Admissions hospitalières / \nHospitalisaties (x 100,000 hab./inw.)", bold(.(period2))))))+
-  #labs(fill=paste0("Admissions hospitalières / \nHospitalisaties (x 100,000 hab./inw.) \n", period2))+
+  labs(fill = bquote(atop(NA, atop("Admissions hospitalières / \nHospitalisaties (x 100,000 hab./inw.)", bold(.(period2)))))) +
   theme_void() +
   theme(
     # Change legend
     legend.position = c(0.2, 0.22),
     legend.title = element_text(size = 12, color = "black"),
-    #legend.title = element_text(size = 9, color = "black"),
     legend.text = element_text(color = "black"),
     plot.margin = unit(c(+0.2, 0, -0.5, 3), "cm")
   )
 
 
 
-
+# save plot
 png(file = "Belgian_Hospitalisations_COVID-19_1.png", width = 15 * 360, heigh = 7 * 360, units = "px", pointsize = 7, res = 300)
-ggarrange(ggarrange(fig_map1,fig_map2, ncol = 1),
-          grid.arrange(fig_trends, bottom = caption),
+ggarrange(ggarrange(fig_map1, fig_map2, ncol = 1),
+  grid.arrange(fig_trends, bottom = caption),
   ncol = 2, widths = c(1, 1.5)
 )
 dev.off()
-
-# plot(map.data$new_in_divid, map.data$new_in_divid2)
-
